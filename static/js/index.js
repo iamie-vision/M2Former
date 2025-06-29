be90718ef3b88de9ef8aec27b4ccb3e7f42474ee
Leto0505 <163614700+panruitao@users.noreply.github.com>
2025-06-29 15:08:07 +0800

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -10,11 +10,11 @@ $(document).ready(function() {
 			loop: true,
 			infinite: true,
 			autoplay: true,
-			autoplaySpeed: 5000,
+			autoplaySpeed: 10000,
     }
 
 		// Initialize all div with carousel class
-    var carousels = bulmaCarousel.attach('.carousel', {autoplaySpeed: 6000});
+    var carousels = bulmaCarousel.attach('.carousel', options);
 	
     bulmaSlider.attach();
 
